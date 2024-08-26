b923b469466d77ff0b3046d1ec49556696840703
Marianna <mariana13019940@gmail.com>
2024-08-26 16:29:16 +0200

    add mount

--- start_docker.sh
+++ start_docker.sh
@@ -12,6 +12,7 @@ docker run --rm --network=host \
   --name $NAME \
   --mount type=bind,source="$LOGS_DIR",target=/opt/spark/logs \
   -v /home:/home \
+  -v /mnt/ceph:/mnt/ceph \
   --entrypoint=sh marianna13/spark-img2dataset \
     $REPO_DIR/start_cluster.sh $MASTER $MASTER_ADDR &
 
