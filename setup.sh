afff150f32cb9e3baf53197686e1bfac069abef4
marianna13 <mariana13019940@gmail.com>
2024-09-09 19:34:47 +0200

    add ray

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 
 # install docker
+CLUSTER_TYPE=$1
 sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER
 newgrp docker
-docker image pull marianna13/ray-img2dataset
+docker image pull marianna13/$CLUSTER_TYPE-img2dataset
 
